69754a7b14abe4c51b8f220856e347cf71525d8a
CharelyBhismaAlzea <148839632+CharelyBhismaAlzea@users.noreply.github.com>
2025-01-10 19:52:42 +0700

    Create experience.php

---
+++ experience.php
@@ -0,0 +1,59 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EIGHTPRO</title>
+    <link rel="stylesheet" href="experience.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
+
+</head>
+
+<body>
+    <header class="header">
+        <a href="#" class="logo"><span>EIGHTPRO</span></a>
+
+        <nav class="navbar">
+            <a href="index.php">HOME</a>
+            <a href="service.php">SERVICES</a>
+            <a href="#" class="active">EXPERIENCE</a>
+            <a href="project.php">PROJECTS</a>
+            <!-- <a href="#">EDUCATION</a> -->
+        </nav>
+
+        <a href="index.php#contact" class="contact">CONTACT US</a>
+    </header>
+
+    <div class="container">
+        <div class="section">
+            <h2>Experience</h2>
+            <p>Adobe<br></p>
+            <p>Dropbox<br></p>
+            <p>Google<br></p>
+            <p>Figma<br></p>
+            <p>Microsoft<br></p>
+        </div>
+
+        <div class="section">
+            <h2>Skills</h2>
+            <p>Product Design</p>
+            <p>UI/UX Design</p>
+            <p>Prototyping</p>
+            <p>Illustration</p>
+            <p>Visual Design</p>
+        </div>
+    </div>
+    <script src="experience.js"></script>
+
+
+</body>
+
+</html>
