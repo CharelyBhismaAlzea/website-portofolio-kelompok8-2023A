177102337b9cad14a87bfe699f5c0e9d9bb669c4
CharelyBhismaAlzea <148839632+CharelyBhismaAlzea@users.noreply.github.com>
2025-01-10 19:52:05 +0700

    Create service.php

---
+++ service.php
@@ -0,0 +1,71 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EIGHTPRO</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="service.css">
+
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
+</head>
+
+<body>
+    <header class="header">
+        <a href="#" class="logo"><span>EIGHTPRO</span></a>
+
+        <nav class="navbar">
+            <a href="index.php">HOME</a>
+            <a href="#" class="active">SERVICES</a>
+            <a href="Experience.php">EXPERIENCE</a>
+            <a href="project.php">PROJECTS</a>
+        </nav>
+
+        <a href="index.php#contact" class="contact">CONTACT US</a>
+    </header>
+
+
+    <div class="container">
+        <h1>Eightpro Your Websolution</h1>
+        <h1>"Kami menyediakan layanan luar biasa untuk klien kami"</h1>
+        <p>Setiap proyek adalah kesempatan untuk menciptakan karya yang luar biasa dan memberikan nilai tambah yang
+            nyata</p>
+        <div class="services">
+            <div class="service">
+                <i class="fas fa-cubes"></i>
+                <h3>Web Design</h3>
+                <p>Web design mencakup berbagai elemen seperti penataan warna, tipografi, gambar, dan grafik,
+                    yang dipadukan untuk menciptakan pengalaman visual yang memikat bagi pengunjung.</p>
+            </div>
+            <div class="service">
+                <i class="fas fa-laptop-code"></i>
+                <h3>Web Application</h3>
+                <p>Web application dirancang untuk menjalankan berbagai fungsi interaktif dan dinamis,
+                    memberikan pengguna kemampuan untuk berinteraksi dengan data dan sistem secara real-time.</p>
+            </div>
+            <div class="service">
+                <i class="fas fa-code"></i>
+                <h3>Web Development</h3>
+                <p>Web Development mencakup berbagai aspek, mulai dari desain antarmuka pengguna (UI)
+                    dan pengalaman pengguna (UX), hingga pengkodean dan pengelolaan server. .</p>
+            </div>
+            <div class="service">
+                <i class="fas fa-ad"></i>
+                <h3>Banner Design</h3>
+                <p>Banner Design menciptakan gambar, grafik, atau elemen visual lainnya
+                    yang digunakan untuk mempromosikan produk, layanan, atau acara.</p>
+          
+        </div>
+    </div>
+    <script src="service.js"></script>
+</body>
+
+</html>
